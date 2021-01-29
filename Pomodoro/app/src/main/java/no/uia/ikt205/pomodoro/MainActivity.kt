799dcd4fc739ad9e536598487a09e8c65b7dda32
Fredrik Hærås <fredrikha@uia.no>
2021-01-29 11:25:34 +0100

    Added new feature

--- Pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ Pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -11,34 +11,80 @@ import no.uia.ikt205.pomodoro.util.millisecondsToDescriptiveTime
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var timer:CountDownTimer
-    lateinit var startButton:Button
-    lateinit var coutdownDisplay:TextView
+    lateinit var timer: CountDownTimer
+    lateinit var startButton: Button
+    lateinit var add30: Button
+    lateinit var add60: Button
+    lateinit var add90: Button
+    lateinit var add120: Button
+    lateinit var countdownDisplay: TextView
 
-    val timeToCountDownInMs = 5000L
+    var timeToCountDownInMs = 0L
     val timeTicks = 1000L
+    val buttonMinuteTimeIncrement = 30L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       startButton = findViewById<Button>(R.id.startCountdownButton)
-       startButton.setOnClickListener(){
-           startCountDown(it)
-       }
-       coutdownDisplay = findViewById<TextView>(R.id.countDownView)
+        startButton = findViewById<Button>(R.id.startCountdownButton)
+        startButton.setOnClickListener() {
+            startCountDown(it)
+        }
 
+        add30 = findViewById<Button>(R.id.add30)
+        add30.setOnClickListener() {
+            Toast.makeText(this@MainActivity, "number 30 is pressed", Toast.LENGTH_SHORT).show()
+            changeCountDownTime(newCountDownTimeinMs = 30*60000)
+        }
+
+        add60 = findViewById<Button>(R.id.add60)
+        add60.setOnClickListener() {
+            Toast.makeText(this@MainActivity, "number 60 is pressed", Toast.LENGTH_SHORT).show()
+            changeCountDownTime(newCountDownTimeinMs = 60*60000)
+        }
+
+        add90 = findViewById<Button>(R.id.add90)
+        add90.setOnClickListener() {
+            Toast.makeText(this@MainActivity, "number 90 is pressed", Toast.LENGTH_SHORT).show()
+            changeCountDownTime(newCountDownTimeinMs = 90*60000)
+        }
+
+        add120 = findViewById<Button>(R.id.add120)
+        add120.setOnClickListener() {
+            Toast.makeText(this@MainActivity, "number 120 is pressed", Toast.LENGTH_SHORT).show()
+            changeCountDownTime(newCountDownTimeinMs = 120*60000)
+        }
+
+        countdownDisplay = findViewById<TextView>(R.id.countDownView)
+
+        /*val setTimeDurationButtons = listOf<Button>(findViewById<Button>(R.id.add30),findViewById<Button>(R.id.setTimeDurationTo60MinutesBt),findViewById<Button>(R.id.setTimeDurationTo90MinutesBt),findViewById<Button>(R.id.setTimeDurationTo120MinutesBt))
+
+        setTimeDurationButtons.forEachIndexed { index, button ->
+            button.setOnClickListener(){
+                if(startButton.isEnabled){
+                    val newCountdownTime = minutesToMilliSeconds((index+1) * buttonMinuteTimeIncrement)
+                    setCountDownTime(newCountdownTime)
+                }
+            }
+        }*/
+
+    }
+
+    fun changeCountDownTime(newCountDownTimeinMs: Long){
+        timeToCountDownInMs = newCountDownTimeinMs
+        updateCountDownDisplay(timeToCountDownInMs)
     }
 
     fun startCountDown(v: View){
 
-        timer = object : CountDownTimer(timeToCountDownInMs,timeTicks) {
+        timer = object : CountDownTimer(timeToCountDownInMs, timeTicks) {
             override fun onFinish() {
                 Toast.makeText(this@MainActivity,"Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
             }
 
             override fun onTick(millisUntilFinished: Long) {
-               updateCountDownDisplay(millisUntilFinished)
+                updateCountDownDisplay(millisUntilFinished)
             }
         }
 
@@ -46,7 +92,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun updateCountDownDisplay(timeInMs:Long){
-        coutdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
+        countdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
     }
 
 }
