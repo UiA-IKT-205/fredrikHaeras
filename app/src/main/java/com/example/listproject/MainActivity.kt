d87e2e5c79a6e8809d8f5a8daaa199ce5d3a8bb2
Fredrik Hærås <fredrikha@uia.no>
2021-04-09 10:43:12 +0200

    Added configuration for firebase. Finished with second screen layout

--- app/src/main/java/com/example/listproject/MainActivity.kt
+++ app/src/main/java/com/example/listproject/MainActivity.kt
@@ -1,22 +1,38 @@
 package com.example.listproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.listproject.databinding.ActivityMainBinding
 import com.example.listproject.mylists.ListCollectionAdapter
 import com.example.listproject.mylists.ListDepositoryManager
 import com.example.listproject.mylists.data.Task
-import kotlinx.android.synthetic.main.activity_main.view.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.card_layout.view.*
+
+class ListHolder{
+    companion object{
+        var PickedList:Task? = null
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG:String = "ListProject:MainActivity"
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth:FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
+        signInAnonymously()
 
         binding.toDoList.layoutManager = LinearLayoutManager(this)
         binding.toDoList.adapter = ListCollectionAdapter(emptyList<Task>(), this::onTaskClicked)
@@ -33,16 +49,33 @@ class MainActivity : AppCompatActivity() {
 
             binding.newList.setText("")
 
-            addList(title)
+            if(title.isEmpty())
+                Log.e(TAG, "You cannot make a list with no name")
+
+            if(title.isNotEmpty())
+                addList(title)
         }
     }
 
     private fun onTaskClicked(task:Task):Unit{
+        ListHolder.PickedList = task
 
+        val intent = Intent(this,ListDetailsActivity::class.java)
+
+        startActivity(intent)
     }
 
     private fun addList(title:String){
         val list = Task(title)
         ListDepositoryManager.instance.addList(list)
+    }
+
+    private fun signInAnonymously(){
+
+        auth.signInAnonymously().addOnSuccessListener {
+            Log.d(TAG,"Login success ${it.user.toString()}")
+        }.addOnFailureListener{
+            Log.e(TAG,"Login failed ", it)
+        }
     }
 }
