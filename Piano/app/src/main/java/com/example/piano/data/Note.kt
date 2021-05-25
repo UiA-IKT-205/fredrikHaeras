44a36753a76868e2b4b7568c59e3f05d82f44352
Fredrik Hærås <fredrikha@uia.no>
2021-05-25 14:32:44 +0200

    Shitty layout, but correct functionality

--- Piano/app/src/main/java/com/example/piano/data/Note.kt
+++
@@ -1,7 +0,0 @@
-package com.example.piano.data
-
-data class Note(val value:String, val start:String, val end:Double){
-    override fun toString(): String{
-        return "Note $value. Started at $start. Total duration $end ms"
-    }
-}
